91324c82a5655cd558d52ceb21ea681a435bbaad
tfuxu <73042332+tfuxu@users.noreply.github.com>
2025-04-26 15:06:13 +0200

    feat: switch to `smrtrfszm/tree-sitter-blueprint` grammar

--- languages/blueprint/highlights.scm
+++ languages/blueprint/highlights.scm
@@ -1,85 +1,135 @@
-; highlights.scm
+(line_comment) @comment
+(block_comment) @comment
 
-(object_id) @variable
+(using
+  namespace: (ident) @module)
 
-(string) @string
+(type_name
+  name: (ident) @type)
+(type_name
+  namespace: (ident) @module)
+
+(object
+  id: (ident) @variable)
+(closure_expression
+  name: (ident) @variable)
+(lookup_expression (ident) @property)
+(child_annotation
+  (child_internal
+    name: (ident) @variable))
+
+(property
+  name: (ident) @property)
+(ext_adw_dialog_response
+  id: (ident) @property)
+(ext_adw_breakpoint
+  property: (ident) @property)
+(ext_combo_box_items
+  id: (ident) @property)
+(ext_accessibility
+  name: (ident) @property)
+(ext_layout
+  name: (ident) @property)
+(menu_attribute
+  name: (ident) @property)
+(ext_adw_breakpoint
+  object: (ident) @property)
+
+(signal
+  handler: (ident) @function)
 
 (escape_sequence) @string.escape
 
-(comment) @comment
+(integer_literal) @number
+(hex_literal) @number
+(float_literal) @number.float
+(quoted) @string
 
-(constant) @constant.builtin
+(flags (ident) @constant)
 
-(boolean) @boolean
+((literal (ident) @constant.builtin)
+  (#eq? @constant.builtin "null"))
+((literal (ident) @boolean)
+  (#any-of? @boolean "true" "false"))
 
-(using) @keyword.import
+(child_annotation
+  child_type: (ident) @attribute)
 
-(template) @keyword
-
-(decorator) @attribute
-
-(property_definition
-  (property_name) @property)
-
-(property_definition
-  (property_binding
-    "bind" @keyword
-    (property_name) @property
-    ["no-sync-create" "bidirectional" "inverted"]* @keyword))
-
-(object) @type
-
-(signal_binding
-  (signal_name) @function.builtin)
-
-(signal_binding
-  (function
-    (identifier)) @function)
-
-(signal_binding
-  "swapped" @keyword)
-
-(styles_list
-  "styles" @function.macro)
-
-(layout_definition
-  "layout" @function.macro)
-
-(gettext_string
-  "_" @function.builtin)
-
-(menu_definition
-  "menu" @keyword)
-
-(menu_section
-  "section" @keyword)
-
-(menu_item
-  "item" @function.macro)
-
-(template_definition
-  (template_name_qualifier) @type.qualifier)
-
-(import_statement
-  (gobject_library) @namespace)
-
-(import_statement
-  (version_number) @number.float)
-
-(float) @number.float
-(number) @number
+"internal-child" @attribute
 
 [
-  ";"
-  "."
-  ","
-] @punctuation.delimiter
+  "action"
+  "response"
+  "default"
+] @attribute
+
+"=" @operator
 
 [
   "("
   ")"
-  "["
-  "]"
   "{"
   "}"
+  "["
+  "]"
+  "<"
+  ">"
 ] @punctuation.bracket
+
+[
+  ":"
+  ";"
+  "."
+  ","
+  "::"
+  "|"
+  "=>"
+] @punctuation.delimiter
+
+[
+  "$"
+  "_"
+  "C_"
+] @punctuation.special
+
+"using" @keyword.import
+
+(binding
+  flags: (ident) @constant)
+(ext_adw_dialog_response
+  flags: (ident) @constant)
+
+(binding
+  flags: (ident) @constant.builtin
+  (#any-of? @constant.builtin "inverted" "bidirectional" "no-sync-create" "sync-create"))
+(ext_adw_dialog_response
+  flags: (ident) @constant.builtin
+  (#any-of? @constant.builtin "destructive" "suggested" "disabled"))
+
+[
+  "template"
+  "menu"
+  "submenu"
+  "section"
+  "item"
+  "accessibility"
+  "styles"
+  "bind"
+  "bind-property"
+  "responses"
+  "condition"
+  "setters"
+  "items"
+  "as"
+  "expr"
+  "mime-types"
+  "patterns"
+  "suffixes"
+  "layout"
+  "marks"
+  "mark"
+  "widgets"
+  "strings"
+  "translation-domain"
+  "typeof"
+] @keyword
